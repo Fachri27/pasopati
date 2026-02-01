b1ec5b478551ae44a41623f1311a2cd1062b0cb8
Fachri <aiti@192.168.1.15>
2026-02-01 18:51:27 +0700

    update meta

--- app/Traits/HasSeoMeta.php
+++ app/Traits/HasSeoMeta.php
@@ -11,25 +11,22 @@ trait HasSeoMeta
         $translation = $this->translations->where('locale', $locale)->first();
 
         // daftar kemungkinan nama field image
-        $imageFields = ['image', 'featured_image'];
+        // $imageFields = ['featured_image', 'image'];
 
-        $imagePath = null;
+        // $imagePath = null;
 
-        // cari field pertama yang ada dan tidak null
-        foreach ($imageFields as $field) {
-            if (! empty($this->{$field})) {
-                $imagePath = $this->{$field};
-                break;
-            }
-        }
+        // // cari field pertama yang ada dan tidak null
+        // foreach ($imageFields as $field) {
+        //     if (! empty($this->{$field})) {
+        //         $imagePath = $this->{$field};
+        //         break;
+        //     }
+        // }
 
         return [
-            'title' => 'Pasopati - '.($translation->title ?? $this->title ?? config('app.name')),
-            'description' => $translation->meta_description
-                ?? Str::limit(strip_tags($translation->deskripsi ?? $translation->content ?? ''), 160),
-            'image' => $imagePath
-                ? asset('storage/'.$imagePath)
-                : asset('img/image.png'),
+            'title' => $translation->title ?? ($this->title ?? config('app.name')),
+            'description' => $translation->meta_description ?? Str::limit(strip_tags($translation->deskripsi ?? $translation->content ?? ''), 160),
+            'image' => isset($this->featured_image) ? asset('storage/' . $this->featured_image) : asset('images/logo.png'),
             'type' => 'article',
         ];
     }
