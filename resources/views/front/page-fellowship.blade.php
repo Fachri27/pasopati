7aa73c405b1e3ab2c09f93bdf54d2913edc5e86a
Fachri <aiti@AITIs-MacBook-Air-2.local>
2026-02-18 19:52:10 +0700

    update

--- resources/views/front/page-fellowship.blade.php
+++ resources/views/front/page-fellowship.blade.php
@@ -5,17 +5,17 @@
         <img src="{{ asset('storage/' . $fellowship->image) }}" alt="poster 1" class="w-full h-auto object-cover">
     </div>
 </section>
-<div x-data="{ selectedId: {{ $categories->first()->id ?? 'null' }} }"
+<div x-data="{ selectedId: {{ $fellowship->kategoris->first() ? (int)$fellowship->kategoris->first()->id : 'null' }} }"
     class="md:flex max-w-screen-lg mx-auto md:mt-10 font-sans">
 
     <!-- Sidebar -->
     <aside class="md:w-64 w-full bg-white md:sticky md:top-0  md:self-start md:p-4 pt-5 px-3">
         <nav class="flex flex-col space-y-1">
-            @foreach ($categories as $category)
+            @foreach ($fellowship->kategoris as $category)
             @php
             $catTrans = $category->translations->first();
             @endphp
-            <button @click="selectedId = {{ $category->id }}" :class="selectedId === {{ $category->id }} 
+            <button @click="selectedId = {{ (int)$category->id }}" :class="selectedId === {{ (int)$category->id }} 
                         ? 'bg-red-600 text-white font-bold' 
                         : 'text-red-600 hover:bg-red-100 font-semibold'" class="px-3 py-2 text-left">
                 {{ $catTrans->kategori_name ?? 'No Title' }}
@@ -26,7 +26,7 @@
 
     <!-- Content -->
     <main class="flex-1 px-5 py-3 md:py-2 text-sm">
-        @foreach ($categories as $category)
+        @foreach ($fellowship->kategoris as $category)
         @php
         $catTrans = $category->translations->first();
         $contentId = $category->pivot->content_id ?? '';
@@ -34,7 +34,8 @@
         $content = $locale === 'id' ? $contentId : ($contentEn ?: $contentId);
         @endphp
 
-        <div x-show="selectedId === {{ $category->id }}" style="display: none !important">
+        <div x-show="selectedId === {{ (int)$category->id }}" :key="{{ (int)$category->id }}"
+            style="display: none !important">
             <div class="prose
       max-w-2xl mx-auto
       px-5
@@ -55,9 +56,8 @@
                 {!! $content !!}
             </div>
         </div>
-</div>
-@endforeach
-</main>
+        @endforeach
+    </main>
 </div>
 
 @include('front.components.floating')
